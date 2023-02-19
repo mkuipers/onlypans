e4a1bc52f8d73ab5c6b4a7ea905b80bd9a3904e6
Max Kuipers <mkuipers@Maxs-MBP.attlocal.net>
2023-02-18 19:40:05 -0600

    chatgpt forgot these model relationships but github copilot didn't. it wrote them for me

--- onlypans/app/models/comment.rb
+++ onlypans/app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :pan
+  belongs_to :user
 
   validates :body, presence: true
 end
--- onlypans/app/models/user.rb
+++ onlypans/app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
   has_many :pans
-
+  has_many :comments
+  
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
          #:rememberable?
