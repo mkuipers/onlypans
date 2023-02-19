e3832d6f1f9c6273dd179b478f4ab06155b6e185
Max Kuipers <mkuipers@Maxs-MBP.attlocal.net>
2023-02-18 19:51:22 -0600

    chatgpt error mixing up what the field storing the text of the comment should be (content/body)

--- onlypans/app/controllers/api/v1/comments_controller.rb
+++ onlypans/app/controllers/api/v1/comments_controller.rb
@@ -24,6 +24,6 @@ class Api::V1::CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:body)
   end
 end
--- onlypans/spec/controllers/api/v1/comments_controller_spec.rb
+++ onlypans/spec/controllers/api/v1/comments_controller_spec.rb
@@ -11,29 +11,29 @@ RSpec.describe Api::V1::CommentsController, type: :controller do
   describe "POST #create" do
     it "creates a new comment" do
       expect {
-        post :create, params: { pan_id: pan.id, comment: { content: "Great pan!" } }, format: :json
+        post :create, params: { pan_id: pan.id, comment: { body: "Great pan!" } }, format: :json
       }.to change { pan.comments.count }.by(1)
     end
 
     it "associates the comment with the current user" do
-      post :create, params: { pan_id: pan.id, comment: { content: "Great pan!" } }, format: :json
+      post :create, params: { pan_id: pan.id, comment: { body: "Great pan!" } }, format: :json
       expect(assigns(:comment).user).to eq(user)
     end
 
     it "returns a successful response" do
-      post :create, params: { pan_id: pan.id, comment: { content: "Great pan!" } }, format: :json
+      post :create, params: { pan_id: pan.id, comment: { body: "Great pan!" } }, format: :json
       expect(response).to have_http_status(:created)
     end
 
     it "returns the comment in the response" do
-      post :create, params: { pan_id: pan.id, comment: { content: "Great pan!" } }, format: :json
-      expect(JSON.parse(response.body)['comment']['content']).to eq("Great pan!")
+      post :create, params: { pan_id: pan.id, comment: { body: "Great pan!" } }, format: :json
+      expect(JSON.parse(response.body)['comment']['body']).to eq("Great pan!")
     end
 
     it "returns an error for invalid parameters" do
-      post :create, params: { pan_id: pan.id, comment: { content: "" } }, format: :json
+      post :create, params: { pan_id: pan.id, comment: { body: "" } }, format: :json
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(JSON.parse(response.body)['errors']).to include("Content can't be blank")
+      expect(JSON.parse(response.body)['errors']).to include("Body can't be blank")
     end
   end
 
