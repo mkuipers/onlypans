138a376bfa776387b80f789a2d5ce8fc51cae186
Max Kuipers <mkuipers@Maxs-MBP.attlocal.net>
2023-02-18 21:42:03 -0600

    remove deprecation warnings

--- onlypans/spec/factories/pans.rb
+++ onlypans/spec/factories/pans.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :pan do
     sequence(:name) { |n| "Pan #{n}" }
     description { 'A great pan' }
-    image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'pan.jpg'), 'image/jpg') }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'support', 'pan.jpg'), 'image/jpeg') }
     user
   end
 end
