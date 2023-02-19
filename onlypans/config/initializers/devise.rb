5892b8e3a8cd4f5a523702d34f9918bd13207fa6
Max Kuipers <mkuipers@Maxs-MBP.attlocal.net>
2023-02-18 21:35:40 -0600

    i think it was my own dumb ass that was screwing this up before

--- onlypans/config/initializers/devise.rb
+++ onlypans/config/initializers/devise.rb
@@ -312,7 +312,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+    jwt.secret = ENV['SECRET_KEY_BASE'] || Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
