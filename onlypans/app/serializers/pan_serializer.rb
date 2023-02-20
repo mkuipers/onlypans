2db1deb8bc2e448b8f62f603e3b74e263284f7bc
Max Kuipers <mkuipers@Maxs-MBP.attlocal.net>
2023-02-20 05:12:49 -0600

    Can post comments about a pan now

--- onlypans/app/serializers/pan_serializer.rb
+++ onlypans/app/serializers/pan_serializer.rb
@@ -2,7 +2,10 @@ class PanSerializer
   include JSONAPI::Serializer
   attributes :name, :description, :rating, :id
 
-  has_many :comments, serializer: CommentSerializer
+  attribute :comments do |object|
+    object.comments.map { |comment| CommentSerializer.new(comment).serializable_hash[:data][:attributes] }
+  end
+  # has_many :comments, serializer: CommentSerializer
 
   attribute :image_url do |object|
     Rails.application.routes.url_helpers.rails_blob_url(object.image, only_path: true) if object.image.attached?
