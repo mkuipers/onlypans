b6dea99434407ba0733b9dd6a1963db5c4cc36c5
Max Kuipers <mkuipers@Maxs-MBP.attlocal.net>
2023-02-20 04:37:55 -0600

    Also serialize all the comments on a pan

--- onlypans/app/serializers/pan_serializer.rb
+++ onlypans/app/serializers/pan_serializer.rb
@@ -1,7 +1,9 @@
 class PanSerializer
   include JSONAPI::Serializer
   attributes :name, :description, :rating, :id
-  
+
+  has_many :comments, serializer: CommentSerializer
+
   attribute :image_url do |object|
     Rails.application.routes.url_helpers.rails_blob_url(object.image, only_path: true) if object.image.attached?
   end
